e01ae0b10b1c430848e6beef9cc0f90123c44570
Albert Huang <alberthuang43@gmail.com>
2014-11-13 17:38:23 +0800

    Add console platform.

---
+++ ConsolePlatform/ConsolePlatform/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePlatform
+{
+    class Program
+    {
+        enum LIBENUM
+        {
+            DEVELOP
+        }
+
+        static void Main(string[] args)
+        {
+            Entry(LIBENUM.DEVELOP);
+        }
+
+        static void Entry(LIBENUM Index)
+        {
+            switch (Index)
+            {
+                case LIBENUM.DEVELOP:
+                    develop();
+                    break;
+            }
+        }
+
+        static void develop()
+        { 
+        }
+    }
+}
